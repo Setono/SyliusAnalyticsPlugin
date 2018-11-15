5b2dc7b724f3f8782e765606e3a9b694782fe707
mati <mateusz.koruna@bitbag.pl>
2018-11-15 19:17:04 +0100

    [wip] add menu

--- src/Entity/GoogleAnalyticConfigInterface.php
+++ src/Entity/GoogleAnalyticConfigInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Setono\SyliusAnalyticsPlugin\Entity;
 
 use Sylius\Component\Resource\Model\ResourceInterface;
---
+++ src/Menu/AnalyticsMenuBuilder.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusAnalyticsPlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AnalyticsMenuBuilder
+{
+    public function addAnalytics(MenuBuilderEvent $event): void
+    {
+        $catalogMenu = $event->getMenu()->getChild('catalog');
+        $catalogMenu
+            ->addChild('analytics', ['route' => 'setono_sylius_analytics_plugin_admin_analytics_index'])
+            ->setLabel('setono_sylius_analytics_plugin.ui.analytics_index')
+            ->setLabelAttribute('icon', 'bullhorn')
+        ;
+    }
+}
