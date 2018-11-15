7db3b7f5a5343db3b887e915249ae74b6bace793
mati <mateusz.koruna@bitbag.pl>
2018-11-15 16:22:25 +0100

    initial commit#2

---
+++ src/SetonoSyliusAnalyticsPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusAnalyticsPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SetonoSyliusAnalyticsPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
