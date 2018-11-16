a3b19aa7a3cf32b470d1417521680da35cb8a1ca
mati <mateusz.koruna@bitbag.pl>
2018-11-16 12:54:12 +0100

    entity,routes fix

--- src/Entity/GoogleAnalyticConfig.php
+++ src/Entity/GoogleAnalyticConfig.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Setono\SyliusAnalyticsPlugin\Entity;
 
-class GoogleAnalyticConfig
+final class GoogleAnalyticConfig implements GoogleAnalyticConfigInterface
 {
     /** @var int */
+    protected $id;
+
+    /** @var int */
     protected $trackingId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getTrackingId(): ?int
     {
--- src/Entity/GoogleAnalyticConfigInterface.php
+++ src/Entity/GoogleAnalyticConfigInterface.php
@@ -8,5 +8,6 @@ use Sylius\Component\Resource\Model\ResourceInterface;
 
 interface GoogleAnalyticConfigInterface extends ResourceInterface
 {
-    public function getTrackingId(): int;
+    public function getId(): ?int;
+    public function getTrackingId(): ?int;
 }
