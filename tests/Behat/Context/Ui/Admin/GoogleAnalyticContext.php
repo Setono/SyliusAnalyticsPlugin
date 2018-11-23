a3e76e2a3f8e0db2068c3ba3014b15f0772810dc
mati <mateusz.koruna@bitbag.pl>
2018-11-23 19:03:07 +0100

    [WIP] Behat , add js and tranlation

---
+++ tests/Behat/Context/Ui/Admin/GoogleAnalyticContext.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Setono\SyliusAnalyticsPlugin\Behat\Context\Ui\Admin;
+
+use Behat\Behat\Context\Context;
+
+final class GoogleAnalyticContext implements Context
+{
+
+}
