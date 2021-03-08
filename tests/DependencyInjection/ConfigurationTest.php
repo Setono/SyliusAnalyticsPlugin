a9d5a98728ed945b6302ee461bc33f617af9dcac
Joachim Løvgaard <joachim@loevgaard.dk>
2021-03-08 14:47:55 +0100

    Remove interface since we don't use it since resource bundle 1.6

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -10,7 +10,6 @@ use Setono\SyliusAnalyticsPlugin\DependencyInjection\Configuration;
 use Setono\SyliusAnalyticsPlugin\Doctrine\ORM\PropertyRepository;
 use Setono\SyliusAnalyticsPlugin\Form\Type\PropertyType;
 use Setono\SyliusAnalyticsPlugin\Model\Property;
-use Setono\SyliusAnalyticsPlugin\Model\PropertyInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Sylius\Component\Resource\Factory\Factory;
@@ -35,7 +34,6 @@ final class ConfigurationTest extends TestCase
                 'property' => [
                     'classes' => [
                         'model' => Property::class,
-                        'interface' => PropertyInterface::class,
                         'controller' => ResourceController::class,
                         'repository' => PropertyRepository::class,
                         'factory' => Factory::class,
