59ee86994546c3773ff495291f6fbc02c459edb3
Joachim Løvgaard <joachim@loevgaard.dk>
2022-08-10 14:48:44 +0200

    Upgrade test application, meta files and deps

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -1,10 +1,12 @@
 <?php
 
-use Tests\Setono\SyliusAnalyticsPlugin\Application\Kernel;
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
+declare(strict_types=1);
 
-require dirname(__DIR__).'/config/bootstrap.php';
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+use Tests\Setono\SyliusAnalyticsPlugin\Application\Kernel;
+
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
