d0ab2ad5901b71b5abcaf7dee766a9a998212cbd
Joachim Løvgaard <joachim@loevgaard.dk>
2023-07-12 13:54:18 +0200

    Add composer-unused.php

---
+++ composer-unused.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use ComposerUnused\ComposerUnused\Configuration\Configuration;
+use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
+
+return static function (Configuration $config): Configuration {
+    return $config
+        ->addNamedFilter(NamedFilter::fromString('sylius/grid-bundle')) // We use this for creating the grids in the backend
+    ;
+};
